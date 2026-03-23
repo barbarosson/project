5a22abb97288a3d45a0fd69a999c503aeaed443b
barbarosson <76139070+barbarosson@users.noreply.github.com>
2026-03-23 15:34:10 +0300

    Revert "feat: integrate BetaHomeGate into HomePageContent"
    This reverts commit aec9c4c8f63083aa1244904a76d2261e74551c57.

--- components/home-page-content.tsx
+++ components/home-page-content.tsx
@@ -1,12 +1,7 @@
 'use client'
 
 import { ModulusLandingLayout } from '@/components/marketing/parasut-landing-layout'
-import { BetaHomeGate } from '@/components/marketing/beta-home-gate'
 
 export function HomePageContent() {
-  return (
-    <BetaHomeGate>
-      <ModulusLandingLayout />
-    </BetaHomeGate>
-  )
+  return <ModulusLandingLayout />
 }
